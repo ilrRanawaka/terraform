16d8e6937e596085e6179bb07e5c44d0332952db
ilrRanawaka <31509398+ilrRanawaka@users.noreply.github.com>
2025-01-22 15:53:51 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-#This is a Test
+resource "port_integration" "terraform-cloud" {
+  installation_id       = "terraform-cloud"
+  installation_app_type = "terraform-cloud"
+  title                 = "Terraform Sync"
+  # version               = "0.1.100"
+  # The reason for the jsonencode|jsondecode is
+  # to preserve the exact syntax as terraform expects,
+  # this resolves conflicts in the state caused by indents
+  config                = jsonencode(jsondecode(file("${path.module}/terraform_sync_tf.json")))
+
+  depends_on = [ module.latam-chart ]
+}
